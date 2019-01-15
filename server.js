46ee1d3fd8758c4b36f1652d0a8c3b2b615cdb77
Sam <rajsam003@gmail.com>
2019-01-15 16:59:07 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+//middleware
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next ) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err)=> {
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    })
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintainance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('kathiSollu', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req,res)=>{
+    // //res.send(<h1>Hello World</h1>);
+    // res.send({
+    //     name: 'Sam',
+    //     age: 24
+    // });
+    res.render('home.hbs',{
+        pageTitle: 'About Sam Page',
+        mairu: 'Poda mairu'
+    })
+});
+
+app.get('/bad', (req,res)=>{
+res.send({
+    errorMessage: '404 ERROR'
+    });
+});
+
+app.get('/about', (req,res)=> {
+    res.render('about.hbs',{
+        pageTitle: 'About Sam Page'
+    });
+});
+
+app.listen(3000, ()=> {
+    console.log('Server is up in 3000');
+});
